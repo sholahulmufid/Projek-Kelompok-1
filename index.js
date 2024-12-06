b4e962c8be00a2b08cbee19de98cd7381ac160d1
sholahulmufid <sholahulmufid@upi.edu>
2024-12-06 19:45:00 +0700

    finish 1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("/html/home.html")
+  fetch("/html/footer.html")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
       return response.text();
     })
     .then((data) => {
-      document.getElementById("home-container").innerHTML = data;
+      document.getElementById("footer-container").innerHTML = data;
     })
     .catch((error) => {
       console.error("Error loading header:", error);
